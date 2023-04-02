fef7891e71286fafb9253b1bcf39fa893b8df4af
jeremygradisher <jeremygradisher@gmail.com>
2023-04-02 02:18:58 -0400

    Default scope on task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,10 @@
 class Task < ApplicationRecord
 
-enum state: {
-initial: 0,
-started: 1,
-done: 2
-}
+    enum state: {
+    initial: 0,
+    started: 1,
+    done: 2
+    }
 
+    default_scope { where("deleted_at is NULL")}
 end
